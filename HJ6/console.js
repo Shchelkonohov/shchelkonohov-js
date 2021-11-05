f706c216619ac6ff491e5d295df8239641fc092f
Shchelkonohov <darta0418@gmail.com`>
2021-11-05 09:48:40 +0200

    almost

---
+++ HJ6/console.js
@@ -0,0 +1,24 @@
+let a;
+a = 3.14;
+console.log(typeof a);
+
+let a1;
+a1 = "a = Number";
+console.log(typeof a1);
+
+let b = 0;
+console.log(typeof b);
+
+let me;
+
+const PI = null;
+console.log(typeof PI);
+
+let y = undefined;
+console.log(typeof y);
+
+let x = Boolean(false);
+console.log(typeof x);
+
+var n = Symbol();
+console.log(typeof n);
