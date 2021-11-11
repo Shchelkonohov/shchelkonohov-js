602cafd6cee4f615b64bda4af324537fcdb7fdd7
Shchelkonohov <darta0418@gmail.com`>
2021-11-11 23:03:18 +0200

    all files

--- HJ8/Tabels/js.js
+++ HJ8/Tabels/js.js
@@ -1,11 +1,13 @@
 const NumbOfTabl = prompt("Введіть кількість столів!(1-100)");
 let x = NumbOfTabl.charAt(NumbOfTabl.length - 1);
-if (x == 1) {
-  console.log(NumbOfTabl + " стіл");
-} else if (x >= 5 || x == 0) {
-  console.log(NumbOfTabl + " столів");
-} else if (x <= 4) {
-  console.log(NumbOfTabl + " столи");
+let y = NumbOfTabl.charAt(NumbOfTabl.length - 2);
+const whole = parseInt(NumbOfTabl, 10);
+if (x == 1 && y != 1) {
+  console.log(whole + " стіл");
+} else if (x >= 5 || x === 0 || y == 1) {
+  console.log(whole + " столів");
+} else if (x <= 4 && x === 0) {
+  console.log(whole + " столи");
 }
 if (NumbOfTabl < 0) {
   console.log("Вас часом не Максим звуть, а то руки чешуться");
