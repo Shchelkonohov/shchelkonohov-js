4e80be4f6e1fc75f117f2d982bcef6bdefa2e041
unknown <darta0418@gmail.com>
2021-12-06 15:20:32 +0200

    all

---
+++ HJ11/object.js
@@ -0,0 +1,9 @@
+let consol = {
+    name: 'Sergiy',
+    message : function () {
+        alert("massage :")
+        }
+    }
+
+console.log(consol);
+
--- HJ8/Tabels/js.js
+++ HJ8/Tabels/js.js
@@ -6,7 +6,7 @@ if (x == 1 && y != 1) {
   console.log(whole + " стіл");
 } else if (x >= 5 || x === 0 || y == 1) {
   console.log(whole + " столів");
-} else if (x <= 4 && x === 0) {
+} else if (x <= 4 ) {
   console.log(whole + " столи");
 }
 if (NumbOfTabl < 0) {
