ae3ab0d01766de5258564c26f55e86fbb2fb8a8b
unknown <darta0418@gmail.com>
2022-01-30 11:56:45 +0200

    all

--- Hj12/arrays.js
+++ Hj12/arrays.js
@@ -1,10 +1,10 @@
 function array_revers(numbers){
  numbers = ["one", "two", "three", "four"]
    let n = numbers.length
-   console.log(n);
-   for(let i = 0;i<n-1;i++){
-const newStrucrute = numbers.unshift(numbers[n-1])
-console.log(numbers);
+   // console.log(n);
+   for(let i = 0;i<n;i++){
+const newStrucrute = numbers
+console.log(newStrucrute);
    }
 
 }
