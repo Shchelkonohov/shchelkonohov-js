d38caf4e1d8153fd02ff6f0f5650f1b8b8bf6882
unknown <darta0418@gmail.com>
2022-02-18 17:49:20 +0200

    all

--- HJ15/Whether1.js
+++ HJ15/Whether1.js
@@ -1,18 +1,73 @@
-let url = "https://api.openweathermap.org/data/2.5/weather?lat=49.838261&lon=24&units=metric&appid=8aff93f121b1c5138acd1735a974912d"
+    let temp = document.querySelector(".first_part")
+    let description = document.querySelector(".second_part")
+    let name = document.querySelector(".third_part")
 
-const getWeather = async () => {
-    let response = await fetch(url);
-    let json = await response.json();
-    console.log(json)
+    let icon = document.querySelector(".icon")
 
-}
-getWeather()
 
-function my_function() {
-    var today = new Date();
-    var date = today.getDate() + '-0' + (today.getMonth() + 1) + '-' + today.getFullYear();
-    document.getElementById('data1').value = date
-    console.log(date)
-    console.log(data1)
-}
-my_function()
+    function getWether() {
+        fetch("https://api.openweathermap.org/data/2.5/weather?lat=49.838261&lon=24&units=metric&appid=8aff93f121b1c5138acd1735a974912d")
+            .then(res => res.json())
+            .then(data => {
+                let nameValue = data['name'];
+                let tempValue = data["main"]['temp']
+                let allValue = data['weather'][0]['description']
+                let iconValue = data['weather'][0]['main']
+                temp.innerHTML = tempValue + ' C°  '
+                description.innerHTML = allValue
+                name.innerHTML = nameValue + ',   ' + nameValue
+                if (iconValue = "Clouds") {
+                    var img = document.createElement("img")
+                    img.src = 'weather-1_icon-icons.com_67740.png';
+                    icon.appendChild(img);
+                }
+
+
+            })
+            .catch(err => alert("smth wrng"))
+
+
+    }
+    getWether()
+
+    function my_function() {
+        var today = new Date();
+        var date = today.getDate() + '-0' + (today.getMonth() + 1) + '-' + today.getFullYear();
+        inside = document.getElementById("data1")
+        inside.innerHTML = date;
+    }
+    my_function()
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // function cloud() {
+    //     let clouds = document.createElement("clouds")
+    //     clouds.src = 'weather-1_icon-icons.com_67740.png'
+    //     let src = document.getElementsByClassName("icon")
+    //     src.appendChild(clouds)
+    // }
+
+    // function snow() {
+    //     let snow = document.createElement("snow")
+    //     snow.src = "png-transparent-rain-and-snow-mixed-weather-computer-icons-snow-leaf-cloud-heart-removebg-preview.png"
+    //     let src = document.getElementsByClassName("icon")
+    //     src.appendChild(snow)
+    // }
+
+    // function sun() {
+    //     let sun = document.createElement("sun")
+    //     sun.src = "Без_названия-removebg-preview.png"
+    //     let src = document.getElementsByClassName("icon")
+    //     src.appendChild(sun)
+
+    // }
